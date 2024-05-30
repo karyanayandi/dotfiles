cf3b457dc1093762de2a25b39555e3118bee6f29
karyanayandi <xkaryanayandi@gmail.com>
2024-05-31 01:17:07 +0700

    update tmux keybind

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -2,20 +2,30 @@
   programs.tmux.enable = true;
   programs.tmux.clock24 = true;
   programs.tmux.plugins = with pkgs.tmuxPlugins; [
-    gruvbox
-    sensible
-    yank
-    resurrect
     better-mouse-mode
+    gruvbox
+    resurrect
+    sensible
+    vim-tmux-navigator
+    yank
     {
       plugin = rose-pine;
       extraConfig = ''
         unbind C-b
+
         set -g prefix C-Space
+        set -g mouse on
+
+        set-option -g status-position top
+
         bind C-Space send-prefix
         bind - split-window -h
         bind '=' split-window -v
-        set -g mouse on
+
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
       '';
     }
   ];
