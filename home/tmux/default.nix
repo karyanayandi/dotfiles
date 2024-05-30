71e0e8e3f8d0bfcdb256fba74010bb91c9b92bab
karyanayandi <xkaryanayandi@gmail.com>
2024-05-31 06:25:38 +0700

    update

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -1,30 +1,71 @@
-{pkgs, ...}: {
-  programs.tmux.enable = true;
-  programs.tmux.clock24 = true;
-  programs.tmux.plugins = with pkgs.tmuxPlugins; [
-    better-mouse-mode
-    gruvbox
-    resurrect
-    sensible
-    vim-tmux-navigator
-    yank
-    {
-      plugin = rose-pine;
-      extraConfig = ''
-        set -g prefix C-Space
-        set -g mouse on
+{pkgs, ...}: let
+  gruvbox = builtins.fetchTarball {
+    url = "https://github.com/z3z1ma/tmux-gruvbox/archive/refs/heads/main.tar.gz";
+    sha256 = "1wlkyzgg56vval97mr1gvg22wf48yvsaahsapg3rrn7r35b93fka";
+  };
+in {
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    extraConfig = ''
+      unbind-key C-b
 
-        set-option -g status-position top
+      set -g prefix C-Space
+      set -g mouse on
+      set -g visual-activity off
+      set -g visual-bell off
+      set -g visual-silence off
+      set -g bell-action none
+      set -g status-justify left
+      setw -g monitor-activity off
 
-        bind C-Space send-prefix
-        bind - split-window -h
-        bind '=' split-window -v
+      set-option -g status-right ""
+      set-option -g allow-rename off
+      set-option -g status-position top
+      set-window-option -g window-status-separator ""
 
-        bind-key h select-pane -L
-        bind-key j select-pane -D
-        bind-key k select-pane -U
-        bind-key l select-pane -R
-      '';
-    }
-  ];
+      bind r source-file ~/.tmux.conf
+      bind c new-window
+      bind s split-window -h
+      bind v split-window -v
+
+      bind -n M-'=' choose-tree
+      bind -n M-w new-window -c '#{pane_current_path}'
+      bind -n M-h previous-window
+      bind -n M-l next-window
+      bind -n M-s split-window -h
+      bind -n M-v split-window -v
+      bind -n M-0 select-window -t:=0
+      bind -n M-1 select-window -t:=1
+      bind -n M-2 select-window -t:=2
+      bind -n M-3 select-window -t:=3
+      bind -n M-4 select-window -t:=4
+      bind -n M-5 select-window -t:=5
+      bind -n M-6 select-window -t:=6
+      bind -n M-7 select-window -t:=7
+      bind -n M-8 select-window -t:=8
+      bind -n M-9 select-window -t:=9
+
+      bind-key C-Space send-prefix
+      bind-key '=' choose-tree
+      bind-key w new-window
+      bind-key h previous-window
+      bind-key l next-window
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
+
+      run-shell ~/.tmux/plugins/tmux-gruvbox/gruvbox.tmux
+    '';
+    plugins = with pkgs.tmuxPlugins; [
+      better-mouse-mode
+      resurrect
+      sensible
+      vim-tmux-navigator
+      yank
+    ];
+  };
+
+  home.file."tmux/plugins/tmux-gruvbox".source = "${gruvbox}";
 }
