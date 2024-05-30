114a4d3e08778ee2afc4047e7a6b02ac9113a5fe
karyanayandi <xkaryanayandi@gmail.com>
2024-05-31 03:17:55 +0700

    use tuigreet

--- system/services/greetd/default.nix
+++ system/services/greetd/default.nix
@@ -1,13 +1,26 @@
-{
+{pkgs, ...}: let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+in {
   services.greetd = {
     enable = true;
     settings = rec {
       initial_session = {
         # command = "Hyprland";
-        command = "$SHELL -l";
         # user = "karyana";
+        command = "${tuigreet} --time --remember --cmd Hyprland";
+        user = "greeter";
       };
       default_session = initial_session;
     };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 }
