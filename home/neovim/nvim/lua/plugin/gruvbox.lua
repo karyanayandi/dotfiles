f7c064c53e719e4df676f0ccda25e6a1e06d3e6e
karyanayandi <xkaryanayandi@gmail.com>
2024-06-02 02:44:47 +0700

    update color

--- home/neovim/nvim/lua/plugin/gruvbox.lua
+++ home/neovim/nvim/lua/plugin/gruvbox.lua
@@ -4,8 +4,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.gruvbox_material_transparent_background = true
-      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_transparent_background = false
+      -- vim.g.gruvbox_material_background = 'hard'
       vim.g.gruvbox_material_dim_inactive_windows = false
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_enable_italic = false
