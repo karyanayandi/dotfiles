2ad7d03727fc39e8e07007cde576e88d539dfdd9
karyanayandi <xkaryanayandi@gmail.com>
2024-06-03 23:32:02 +0700

    add custom key for gitui

--- home/git/default.nix
+++ home/git/default.nix
@@ -13,5 +13,17 @@
   };
 
   programs.gh.enable = true;
-  programs.gitui.enable = true;
+  programs.gitui = {
+    enable = true;
+    keyConfig = ''
+      exit: Some(( code: Char('c'), modifiers: ( bits: 2,),)),
+      quit: Some(( code: Char('q'), modifiers: ( bits: 0,),)),
+      exit_popup: Some(( code: Esc, modifiers: ( bits: 0,),)),
+      move_left: Some(( code: Char('h'), modifiers: "")),
+      move_right: Some(( code: Char('l'), modifiers: "")),
+      move_up: Some(( code: Char('k'), modifiers: "")),
+      move_down: Some(( code: Char('j'), modifiers: "")),
+      stage_unstage_item: Some(( code: Char('Space'), modifiers: "")),
+    '';
+  };
 }
