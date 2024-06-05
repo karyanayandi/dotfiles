49e7bb5c7b3069b8bc8382114044ebd5740fb3e2
karyanayandi <xkaryanayandi@gmail.com>
2024-06-05 09:03:48 +0700

    add lock

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
     ./git
     ./gtk
     ./direnv
+    ./lock
     ./neovim
     ./packages
     ./hyprland
---
+++ home/lock/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [swayidle];
+
+  settings.lock = {
+    enable = true;
+    bin = "swaylock";
+    idle = lib.concatStringsSep " " [
+      "swayidle -w"
+      "timeout 450 'swaylock -f'"
+      "timeout 10 'if pgrep swaylock; then hyprctl dispatch dpms off; fi'"
+      "resume 'hyprctl dispatch dpms on'"
+      "before-sleep 'swaylock -f'"
+    ];
+  };
+
+  programs.swaylock = {
+    enable = true;
+    package = pkgs.swaylock-effects;
+    settings =
+      {
+        ignore-empty-password = true;
+        indicator = true;
+        indicator-idle-visible = true;
+        indicator-caps-lock = true;
+        indicator-radius = 100;
+        indicator-thickness = 16;
+        line-uses-inside = true;
+        effect-blur = "9x7";
+        effect-vignette = "0.85:0.85";
+        fade-in = 0.1;
+      }
+      // lib.optionalAttrs (config.settings.wallpaper.${config.settings.scheme} != null) {
+        image = config.settings.wallpaper.${config.settings.scheme};
+      };
+  };
+}
