08457a7f50906d4a60152b6a3f2c568a8fcf9574
karyanayandi <xkaryanayandi@gmail.com>
2024-06-05 09:03:37 +0700

    add power menu

--- home/scripts/power-menu.sh
+++ home/scripts/power-menu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+entries="Logout\nSuspend\nReboot\nShutdown"
+placeholder="Select an action"
 
-selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+selected=$(echo -e $entries | wofi --dmenu --cache-file /dev/null --prompt "$placeholder" | awk -F '\t' '{print tolower($2)}')
 
 case $selected in
   logout)
