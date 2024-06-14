35ab21cc788b23f15c65b66bf837b4107480facf
karyanayandi <xkaryanayandi@gmail.com>
2024-06-14 15:19:25 +0700

    remove unused functions

--- home/neovim/nvim/lua/config/functions.lua
+++ home/neovim/nvim/lua/config/functions.lua
@@ -1,59 +1,9 @@
 local M = {}
 
-vim.cmd [[
-  function Test()
-    %SnipRun
-    call feedkeys("\<esc>`.")
-  endfunction
-
-  function TestI()
-    let b:caret = winsaveview()
-    %SnipRun
-    call winrestview(b:caret)
-  endfunction
-]]
-
-function M.sniprun_enable()
-  vim.cmd [[
-    %SnipRun
-
-    augroup _sniprun
-     autocmd!
-     autocmd TextChanged * call Test()
-     autocmd TextChangedI * call TestI()
-    augroup end
-  ]]
-  vim.notify "Enabled SnipRun"
-end
-
-function M.disable_sniprun()
-  M.remove_augroup "_sniprun"
-  vim.cmd [[
-    SnipClose
-    SnipTerminate
-    ]]
-  vim.notify "Disabled SnipRun"
-end
-
-function M.toggle_sniprun()
-  if vim.fn.exists "#_sniprun#TextChanged" == 0 then
-    M.sniprun_enable()
-  else
-    M.disable_sniprun()
-  end
-end
-
 function M.remove_augroup(name)
   if vim.fn.exists("#" .. name) == 1 then
     vim.cmd("au! " .. name)
   end
-end
-
-vim.cmd [[ command! SnipRunToggle execute 'lua require("config.functions").toggle_sniprun()' ]]
-
-function M.get_word_length()
-  local word = vim.fn.expand "<cword>"
-  return #word
 end
 
 function M.toggle_option(option)
@@ -77,6 +27,7 @@ function M.toggle_tabline()
 end
 
 local diagnostics_active = true
+
 function M.toggle_diagnostics()
   diagnostics_active = not diagnostics_active
   if diagnostics_active then
@@ -84,10 +35,6 @@ function M.toggle_diagnostics()
   else
     vim.diagnostic.hide()
   end
-end
-
-function M.isempty(s)
-  return s == nil or s == ""
 end
 
 function M.get_buf_option(opt)
