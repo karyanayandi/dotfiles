01ac7125d756e3517beae61df221656f2e79e315
karyanayandi <xkaryanayandi@gmail.com>
2024-06-14 19:43:57 +0700

    fix lsp keymap

---
+++ home/neovim/nvim/lua/plugin/lsp/mason.lua
@@ -0,0 +1,58 @@
+local icons = require "config.icons"
+
+local servers = {
+  "astro",
+  "clangd",
+  "bashls",
+  "cssls",
+  "html",
+  "jsonls",
+  "svelte",
+  "tailwindcss",
+  "tsserver",
+  "yamlls",
+  -- "deno",
+  -- "stylua",
+  -- "shellcheck",
+  -- "shfmt",
+  -- "flake8",
+}
+
+local settings = {
+  ui = {
+    border = "none",
+    icons = {
+      package_installed = icons.ui.Installed,
+      package_pending = icons.ui.Pending,
+      package_uninstalled = icons.ui.Uninstalled,
+    },
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
+}
+
+require("mason").setup(settings)
+require("mason-lspconfig").setup {
+  ensure_installed = servers,
+  automatic_installation = true,
+}
+
+local lspconfig = require "lspconfig"
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
+    on_attach = require("plugin.lsp.handlers").on_attach,
+    capabilities = require("plugin.lsp.handlers").capabilities,
+  }
+
+  server = vim.split(server, "@")[1]
+
+  local require_ok, conf_opts = pcall(require, "plugin.lsp.settings." .. server)
+  if require_ok then
+    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+  end
+
+  lspconfig[server].setup(opts)
+end
