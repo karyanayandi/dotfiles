02071cc18e3929f6a83a01de8a4bd61dcc03d437
karyanayandi <xkaryanayandi@gmail.com>
2024-07-01 19:26:26 +0700

    update aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ export STARSHIP_CONFIG=/home/karyana/.config/starship.toml
 alias ls "exa --group-directories-first --icons"
 alias lsl "exa --group-directories-first -lh --icons"
 alias ll "exa -al -g --icons"
-alias tree "exa --tree --level=2 --group-directories-first --icons --ignore-glob='*node_modules*'"
+alias tree "exa --tree --group-directories-first --icons --ignore-glob='*node_modules*'"
 alias vis "sudo nvim"
 alias font-refresh "fc-cache -fv"
 alias clone "git clone --depth 1"
@@ -24,11 +24,13 @@ alias update-lock "betterlockscreen -u ~/.wallpaper.png --fx dim,pixel,blur"
 alias update-time "sudo ntpd -qg"
 alias refresh-mirror "sudo reflector --latest 5 --protocol https --country 'Singapore' --sort rate --save /etc/pacman.d/mirrorlist"
 
+alias exp "gh copilot explain"
+alias sug "gh copilot suggest"
+
 alias sshk "kitty +kitten ssh"
 
 alias x exit
 alias e nvim
-alias nv nvim
 alias vim nvim
 
 alias c clear
@@ -52,15 +54,6 @@ alias vi3 "nvim ~/.config/i3/config"
 alias vpo "nvim ~/.config/polybar/config"
 
 alias g git
-alias gi "git init"
-alias ga "git add . "
-alias gcm "git commit -m"
-alias gbm "git branch -M main"
-alias gpso "git push origin main"
-alias gplo "git pull origin main"
-alias gst "git status"
-alias glg "git log"
-alias gpr "gh pr create"
 
 alias yarn "corepack yarn"
 alias yarnpkg "corepack yarnpkg"
@@ -69,26 +62,8 @@ alias pnpx "corepack pnpx"
 alias npm "corepack npm"
 alias npx "corepack npx"
 
-alias yi "corepack yarn install"
-alias ys "corepack yarn start"
-alias yd "corepack yarn dev"
-alias ya "corepack yarn add"
-alias yad "corepack yarn add -D"
-
 alias pn "corepack pnpm"
-alias pni "corepack pnpm install"
-alias pns "corepack pnpm start"
-alias pnd "corepack pnpm dev"
-alias pna "corepack pnpm add"
-alias pnad "corepack pnpm add -D"
-alias pnr "corepack pnpm remove"
-alias pnu "corepack pnpm up --latest"
-
-alias ni "corepack npm install"
-alias nrs "corepack npm run start"
-alias nrd "corepack npm run dev"
-alias nid "corepack npm install --save-dev"
-alias nig "corepack npm install --location=global"
+alias px "corepack pnpx"
 
 alias lzg lazygit
 
