18d4f0806352590d206668894dd52dc52ee79b24
karyanayandi <xkaryanayandi@gmail.com>
2024-07-17 21:43:42 +0700

    add dokploy cli

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -8,7 +8,7 @@
     export NODE_OPTIONS=--max_old_space_size=4096
     npm config set prefix "${config.home.homeDirectory}/.cache/npm/global"
     mkdir -p "${config.home.homeDirectory}/.cache/npm/global"
-    npm install -g node-gyp ls_emmet
+    npm install -g node-gyp ls_emmet @dokploy/cli
   '';
 in {
   home.activation.configureNode = ''
