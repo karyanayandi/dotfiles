5da49e8a4cded38595d87267831030a8d31cd78d
karyanayandi <xkaryanayandi@gmail.com>
2024-08-13 19:06:03 +0700

    fix wayland

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,10 +1,8 @@
-{pkgs, ...}: {
+{
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
-      source = "${pkgs.writeText "rose-pine.conf" (builtins.readFile "/home/karyana/.config/dotfiles/home/hyprland/rose-pine.conf")}";
-
       monitor = ",highres,auto,1";
       "exec" = "gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'";
 
@@ -12,10 +10,26 @@
         force_zero_scaling = true;
       };
 
+      env = [
+        "XCURSOR_THEME,Bibata-Modern-Ice"
+        "XCURSOR_SIZE,20"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "QT_QPA_PLATFORM,wayland"
+        "QT_QPA_PLATFORMTHEME,qt5ct"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "MOZ_ENABLE_WAYLAND,1"
+        "WLR_NO_HARDWARE_CURSORS,1"
+        "HYPRLAND_NO_SD_NOTIFY=0"
+      ];
+
       "exec-once" = [
         "dunst"
-        "swaybg -i $HOME/Downloads/lake.jpg"
+        "swaybg -i $HOME/.config/dotfiles/wallpaper.jpg"
         "waybar"
+        "hyprctl setcursor Bibata-Modern-Ice 20"
       ];
 
       input = {
@@ -28,7 +42,11 @@
         };
         accel_profile = "adaptive";
         sensitivity = 0.6;
-        kb_options = "caps:escape";
+      };
+
+      gestures = {
+        workspace_swipe = true;
+        worspace_swipe_fingers = 3;
       };
 
       general = {
@@ -36,44 +54,49 @@
         gaps_in = 6;
         gaps_out = 12;
         border_size = 1;
-        "col.active_border" = "$rose";
-        "col.inactive_border" = "$muted";
+        "col.active_border" = "rgba(88c0d0ff)";
+        "col.inactive_border" = "0xff434c5e";
         layout = "dwindle";
       };
 
+      misc = {
+        force_default_wallpaper = false;
+      };
+
       decoration = {
-        rounding = 4;
+        rounding = 3;
+        dim_special = 0.7;
+        blur = {
+          special = false;
+          enabled = false;
+          size = 8;
+          passes = 2;
+        };
       };
 
       animations = {
         enabled = 1;
         animation = [
-          "workspaces,1,4,default"
-          "windows,1,5,overshot,slide"
-          "windowsOut,1,4,smoothOut,slide"
-          "windowsMove,1,4,default"
-          "border,1,10,default"
-          "fade,1,10,smoothIn"
-          "fadeDim,1,10,smoothIn"
-          "workspaces,1,6,default"
+          "windows, 1, 6, wind, popin"
+          "windowsIn, 1, 1, workIn, popin"
+          "windowsOut, 1, 5, workIn, popin"
+          "windowsMove, 1, 5, wind, slide"
+          "fadeIn, 1, 2, winIn"
+          "fadeOut, 1, 5, winOut"
+          "workspaces, 1, 3, workIn, slidevert"
+          "specialWorkspace, 1, 5, workIn, slidevert"
         ];
         bezier = [
-          "overshot,0.05,0.9,0.1,1.05"
-          "smoothOut,0.36,0,0.66,-0.56"
-          "smoothIn,0.25,1,0.5,1"
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.76, 0.42, 0.74, 0.87"
+          "winOut, 0.76, 0.42, 0.74, 0.87"
+          "workIn, 0.72, -0.07, 0.41, 0.98"
+          "linear, 1, 1, 1, 1"
         ];
       };
-
       dwindle = {
         pseudotile = 1;
       };
-
-      gestures = {
-        workspace_swipe = true;
-        workspace_swipe_invert = true;
-        workspace_swipe_min_speed_to_force = 6;
-      };
-
       windowrule = [
         "float,file_progress"
         "float,confirm"
@@ -107,64 +130,103 @@
       ];
 
       windowrulev2 = [
-        "opacity 0.9 0.9,class:^(foot)$"
+        "opacity 0.9 0.9,class:^(code)$"
         "stayfocused,class:(wofi)"
         "noborder,class:(wofi)"
+        "float,class:^(net.davidotek.pupgui2)$ #ProtonUp-Qt"
+        "float,class:^(yad)$ #Protontricks-Gtk"
+        "float,class:^(qt5ct)$"
+        "float,class:^(nwg-look)$"
+        "float,class:^(org.kde.ark)$"
+        "float,class:^(pavucontrol)$"
+        "float,class:^(blueman-manager)$"
+        "float,class:^(nm-applet)$"
+        "float,class:^(nm-connection-editor)$"
+        "float,class:^(org.kde.polkit-kde-authentication-agent-1)$"
+        "float,class:^(com.obsproject.Studio)$,title:^(Controls)$"
+        "blur,logout"
       ];
 
       bind = [
-        "SUPER,Return,exec,foot"
-        "SUPERSHIFT,Q,killactive,"
-        "SUPER,V,togglefloating,"
-        "SUPER,C,exec,grimblast --notify copy screen --propmt=RUN,"
-        "SUPERSHIFT,C,exec,grimblast --notify copy area --propmt=RUN,"
-        "SUPER,SPACE,exec,wofi -S run --prompt=Run"
-        "SUPER,F,fullscreen,0"
-        "SUPER,h,movefocus,l"
-        "SUPER,l,movefocus,r"
-        "SUPER,k,movefocus,u"
-        "SUPER,j,movefocus,d"
-        "SUPER,mouse_down,workspace,e+1"
-        "SUPER,mouse_up,workspace,e-1"
-        "SUPER,g,togglegroup"
-        "SUPER,tab,changegroupactive"
-        "SUPERSHIFT,E,exec,hyprctl dispatch exit"
-        "SUPER CTRL,left,resizeactive,-20 0"
-        "SUPER CTRL,right,resizeactive,20 0"
-        "SUPER CTRL,up,resizeactive,0 -20"
-        "SUPER CTRL,down,resizeactive,0 20"
-        "SUPERSHIFT,h,movewindow,l"
-        "SUPERSHIFT,l,movewindow,r"
-        "SUPERSHIFT,k,movewindow,u"
-        "SUPERSHIFT,j,movewindow,d"
-        "SUPER,1,workspace,1"
-        "SUPER,2,workspace,2"
-        "SUPER,3,workspace,3"
-        "SUPER,4,workspace,4"
-        "SUPER,5,workspace,5"
-        "SUPER,6,workspace,6"
-        "SUPER,7,workspace,7"
-        "SUPER,8,workspace,8"
-        "SUPER,9,workspace,9"
-        "SUPERSHIFT,1,movetoworkspacesilent,1"
-        "SUPERSHIFT,2,movetoworkspacesilent,2"
-        "SUPERSHIFT,3,movetoworkspacesilent,3"
-        "SUPERSHIFT,4,movetoworkspacesilent,4"
-        "SUPERSHIFT,5,movetoworkspacesilent,5"
-        "SUPERSHIFT,6,movetoworkspacesilent,6"
-        "SUPERSHIFT,7,movetoworkspacesilent,7"
-        "SUPERSHIFT,8,movetoworkspacesilent,8"
-        "SUPERSHIFT,9,movetoworkspacesilent,9"
-        ",XF86MonBrightnessUp,exec,brightnessctl set +2%"
-        ",XF86MonBrightnessDown,exec,brightnessctl set 2%-"
-        ",XF86AudioRaiseVolume,exec,amixer -q set Master 2%+ unmute"
-        ",XF86AudioLowerVolume,exec,amixer -q set Master 2%- unmute"
-        ",XF86AudioMute,exec,amixer -q set Master toggle"
+        "SUPER, C, killactive"
+        "SUPER SHIFT, V, togglefloating"
+        "ALT, F4, exec, hyprctl kill"
+        "ALT, P, pin"
+        "SUPER, F, fullscreen"
+        "SUPER SHIFT, Q, exec, power-menu"
+        "SUPER, U, exec, swaylock"
+        "SUPER, Return, exec, foot"
+        "SUPER, B, exec, google-chrome-stable"
+        "SUPER, W, exec, firefox"
+        "SUPER, N, exec, nemo"
+        "SUPER, V, exec, code"
+        "SUPER, SPACE, exec, wofi -S run --prompt=Run"
+        "SUPER SHIFT, SPACE, exec, wofi -S drun --prompt=Run"
+        ", XF86AudioRaiseVolume, exec, amixer -q set Master 2%+ unmute"
+        ", XF86AudioLowerVolume, exec, amixer -q set Master 2%- unmute"
+        ", XF86AudioMute, exec, amixer -q set Master toggle"
+        ", XF86MonBrightnessUp, exec, brightnessctl set +2%"
+        ", XF86MonBrightnessDown, exec, brightnessctl set 2%-"
+        ", Print, exec, grimblast --notify copy screen --prompt=RUN"
+        "SUPER ,Print, exec, grimblast --notify copy area --prompt=RUN"
+        "SUPER, G, exec, screen-record"
+        "SUPERSHIFT, G, exec, stop-screen-record"
+        "SUPER, H, movefocus, l"
+        "SUPER, J, movefocus, d"
+        "SUPER, K, movefocus, u"
+        "SUPER, L, movefocus, r"
+        "SUPER, 1, exec, hyprctl dispatch workspace 1"
+        "SUPER, 2, exec, hyprctl dispatch workspace 2"
+        "SUPER, 3, exec, hyprctl dispatch workspace 3"
+        "SUPER, 4, exec, hyprctl dispatch workspace 4"
+        "SUPER, 5, exec, hyprctl dispatch workspace 5"
+        "SUPER, 6, exec, hyprctl dispatch workspace 6"
+        "SUPER, 7, exec, hyprctl dispatch workspace 7"
+        "SUPER, 8, exec, hyprctl dispatch workspace 8"
+        "SUPER, 9, exec, hyprctl dispatch workspace 9"
+        "SUPER, 0, exec, hyprctl dispatch workspace 10"
+        "SUPER CTRL, right, workspace, r+1"
+        "SUPER CTRL, left, workspace, r-1"
+        "SUPER CTRL, down, workspace, empty"
+        "SUPER SHIFT, h, movewindow, l"
+        "SUPER SHIFT, l, movewindow, r"
+        "SUPER SHIFT, k, movewindow, u"
+        "SUPER SHIFT, j, movewindow, d"
+        "SUPER SHIFT, 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4, movetoworkspace, 4"
+        "SUPER SHIFT, 5, movetoworkspace, 5"
+        "SUPER SHIFT, 6, movetoworkspace, 6"
+        "SUPER SHIFT, 7, movetoworkspace, 7"
+        "SUPER SHIFT, 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9, movetoworkspace, 9"
+        "SUPER SHIFT, 0, movetoworkspace, 10"
+        "SUPER, mouse_down, workspace, e+1"
+        "SUPER, mouse_up, workspace, e-1"
+        "SUPER, Y, togglesplit, # dwindle"
+        "SUPER ALT, 1, movetoworkspacesilent, 1"
+        "SUPER ALT, 2, movetoworkspacesilent, 2"
+        "SUPER ALT, 3, movetoworkspacesilent, 3"
+        "SUPER ALT, 4, movetoworkspacesilent, 4"
+        "SUPER ALT, 5, movetoworkspacesilent, 5"
+        "SUPER ALT, 6, movetoworkspacesilent, 6"
+        "SUPER ALT, 7, movetoworkspacesilent, 7"
+        "SUPER ALT, 8, movetoworkspacesilent, 8"
+        "SUPER ALT, 9, movetoworkspacesilent, 9"
+        "SUPER ALT, 0, movetoworkspacesilent, 10"
+      ];
+
+      binde = [
+        "SUPER SHIFT, D, resizeactive, 30 0"
+        "SUPER SHIFT, A, resizeactive, -30 0"
+        "SUPER SHIFT, W, resizeactive, 0 -30"
+        "SUPER SHIFT, S, resizeactive, 0 30"
       ];
 
       bindm = [
-        "SUPER,mouse:272,movewindow"
-        "SUPER,mouse:273,resizewindow"
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
       ];
     };
   };
