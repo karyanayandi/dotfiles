8bd2936b8919afda46b0576a419da038e945ea8b
karyanayandi <xkaryanayandi@gmail.com>
2024-11-20 10:17:59 +0700

    use zen browser flake fork temporary

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:bodenlosus/zen-browser-flake";
     alejandra = {
       url = "github:kamadorueda/alejandra/3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
