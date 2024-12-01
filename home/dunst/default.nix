901ca727f84b077b0689ab1ef5afaf912a54739b
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 23:56:11 +0700

    customize dunst

--- home/dunst/default.nix
+++ home/dunst/default.nix
@@ -1,31 +1,26 @@
-{pkgs, ...}: {
+{...}: {
   services.dunst = {
     enable = true;
-    iconTheme = {
-      package = pkgs.tela-icon-theme;
-      name = "Tela-dark";
-    };
     settings = {
       global = {
-        origin = "bottom-right";
-        geometry = "320x120-28-28";
-        separator_height = 0;
-        padding = 32;
+        origin = "top-right";
+        offset = "60x12";
+        separator_height = 2;
+        padding = 12;
         horizontal_padding = 12;
         text_icon_padding = 12;
         frame_width = 4;
+        separator_color = "frame";
         idle_threshold = 120;
-        show_age-threshole = 60;
         line_height = 0;
         format = ''
           <b>%s</b>
           %b'';
-        alignment = "left";
+        alignment = "center";
         icon_position = "off";
         startup_notification = "false";
-        corner_radius = 10;
+        corner_radius = 12;
         timeout = 2;
-        hide_duplicate_count = false;
       };
     };
   };
