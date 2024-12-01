638caf37452c200d37ce7ed85271575894ca317c
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 15:11:37 +0700

    change wallpaper

--- system/stylix/default.nix
+++ system/stylix/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
-    image = ../../wallpapers/gruvbox-anime-landscape.png;
+    image = ../../wallpapers/od_error.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
     fonts = {
       sizes = {
