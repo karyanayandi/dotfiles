dcd15496fe50613dd475219257015ef1e961efab
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 13:59:07 +0700

    change waybar font to zed mono

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -61,7 +61,7 @@
     style = ''
       * {
         border: none;
-        font-family: "Inter Variable";
+        font-family: "ZeddMono NF";
         font-size: 15px;
         min-height: 30px;
       }
