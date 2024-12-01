866a4cdfb151bd381239d81b4327ba845d8215e5
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 23:28:16 +0700

    fix hypridle

--- home/lock/default.nix
+++ home/lock/default.nix
@@ -60,31 +60,31 @@
           }
         ];
       };
-      hypridle = {
-        enable = true;
-        settings = {
-          general = {
-            before_sleep_cmd = "hyprlock";
-            after_sleep_cmd = "hyprctl dispatch dpms on";
-            lock_cmd = "hyprlock";
-          };
-
-          listener = [
-            {
-              timeout = 300;
-              on-timeout = "hyprlock";
-            }
-            {
-              timeout = 380;
-              on-timeout = "hyprctl dispatch dpms off";
-              on-resume = "hyprctl dispatch dpms on";
-            }
-            {
-              timeout = 600;
-              on-timeout = "systemctl suspend";
-            }
-          ];
+    };
+    hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          before_sleep_cmd = "hyprlock";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          lock_cmd = "hyprlock";
         };
+
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 380;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 600;
+            on-timeout = "systemctl suspend";
+          }
+        ];
       };
     };
   };
