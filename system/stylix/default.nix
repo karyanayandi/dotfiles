9c50036e8ea973c2c9575aef0749eb761bbae24b
karyanayandi <xkaryanayandi@gmail.com>
2024-12-02 01:01:54 +0700

    use my custom color

--- system/stylix/default.nix
+++ system/stylix/default.nix
@@ -2,7 +2,24 @@
   stylix = {
     enable = true;
     image = ../../wallpapers/onedark/od_outrun_wave.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+    override = {
+      base00 = "#282c34";
+      base01 = "#353b45";
+      base02 = "#3e4451";
+      base03 = "#545862";
+      base04 = "#565c64";
+      base05 = "#abb2bf";
+      base06 = "#b6bdca";
+      base07 = "#c8ccd4";
+      base08 = "#D57780";
+      base09 = "#d19a66";
+      base0A = "#e5c07b";
+      base0B = "#98c379";
+      base0C = "#88C0D0";
+      base0D = "#81A1C1";
+      base0E = "#B988B0";
+      base0F = "#D08F70";
+    };
     fonts = {
       sizes = {
         applications = 10;
