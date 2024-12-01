b86294e14009e41138ea84a43961b1d9dce70e71
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 13:57:27 +0700

    change neovim theme and wallpaper to gruvbox

--- system/stylix/default.nix
+++ system/stylix/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
-    image = ../../wallpapers/anime-skull.png;
+    image = ../../wallpapers/gruvbox-anime-lanscape.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     fonts = {
       sizes = {
