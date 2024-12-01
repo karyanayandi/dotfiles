71dcaa7f3d2ba31bfd121008a16ace8c55c24177
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 23:59:10 +0700

    customize dunst

--- home/dunst/default.nix
+++ home/dunst/default.nix
@@ -3,23 +3,22 @@
     enable = true;
     settings = {
       global = {
-        origin = "top-right";
+        origin = "bottom-right";
         offset = "60x12";
         separator_height = 2;
         padding = 12;
         horizontal_padding = 12;
         text_icon_padding = 12;
-        frame_width = 4;
-        separator_color = "frame";
+        frame_width = 0;
         idle_threshold = 120;
-        line_height = 0;
+        line_height = 12;
         format = ''
           <b>%s</b>
           %b'';
         alignment = "center";
         icon_position = "off";
         startup_notification = "false";
-        corner_radius = 12;
+        corner_radius = 4;
         timeout = 2;
       };
     };
