eaad50d7615b0acaf14b6c5066f37e634dd3ecaf
karyanayandi <xkaryanayandi@gmail.com>
2024-12-01 13:58:30 +0700

    fix typo

--- system/stylix/default.nix
+++ system/stylix/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
-    image = ../../wallpapers/gruvbox-anime-lanscape.png;
+    image = ../../wallpapers/gruvbox-anime-landscape.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     fonts = {
       sizes = {
