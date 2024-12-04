95a2d2ef0464db587600d4e3fa40b943fd27a716
karyanayandi <xkaryanayandi@gmail.com>
2024-12-04 20:01:00 +0700

    refactor conform nvim

--- home/neovim/nvim/lua/plugin/conform.lua
+++ home/neovim/nvim/lua/plugin/conform.lua
@@ -11,44 +11,45 @@ return {
       return vim.fn.glob "deno.json" ~= ""
     end
 
+    local function formatter_for_js_or_ts()
+      return has_deno_json() and { "deno_fmt" } or { "prettierd" }
+    end
+
     local formatters_by_ft = {
-      astro = has_deno_json() and { "deno_fmt", stop_after_first = true } or { "prettierd", stop_after_first = true },
+      astro = formatter_for_js_or_ts(),
       c = { "clang_format" },
-      css = { "prettierd", stop_after_first = true },
+      css = { "prettierd" },
       fish = { "fish_indent" },
       go = { "gofumpt", "goimports-reviser", "golines" },
       graphql = { "prettierd" },
       html = { "prettierd" },
-      javascript = has_deno_json() and { "deno_fmt", stop_after_first = true }
-        or { "prettierd", stop_after_first = true },
-      javascriptreact = has_deno_json() and { "deno_fmt", stop_after_first = true }
-        or { "prettierd", stop_after_first = true },
-      json = { "prettierd", stop_after_first = true },
+      javascript = formatter_for_js_or_ts(),
+      javascriptreact = formatter_for_js_or_ts(),
+      json = { "prettierd" },
       lua = { "stylua" },
-      markdown = has_deno_json() and { "deno_fmt", stop_after_first = true }
-        or { "prettierd", stop_after_first = true },
+      markdown = formatter_for_js_or_ts(),
       nix = { "alejandra" },
-      php = { "phpcs_fixer", "prettierd", stop_after_first = true },
+      php = { "phpcs_fixer", "prettierd" },
       python = { "isort", "black" },
       rust = { "rushfmt" },
       sh = { "shfmt" },
-      svelte = has_deno_json() and { "deno_fmt", stop_after_first = true } or { "prettierd", stop_after_first = true },
-      typescript = has_deno_json() and { "deno_fmt", stop_after_first = true }
-        or { "prettierd", stop_after_first = true },
-      typescriptreact = has_deno_json() and { "deno_fmt", stop_after_first = true }
-        or { "prettierd", stop_after_first = true },
-      vue = has_deno_json() and { "deno_fmt", stop_after_first = true } or { "prettierd", stop_after_first = true },
+      svelte = formatter_for_js_or_ts(),
+      typescript = formatter_for_js_or_ts(),
+      typescriptreact = formatter_for_js_or_ts(),
+      vue = formatter_for_js_or_ts(),
       yaml = { "prettierd", stop_after_first = true },
     }
 
+    local function format_on_save(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true, async = false }
+    end
+
     conform.setup {
       formatters_by_ft = formatters_by_ft,
-      format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_fallback = true, async = false }
-      end,
+      format_on_save = format_on_save,
     }
   end,
 }
