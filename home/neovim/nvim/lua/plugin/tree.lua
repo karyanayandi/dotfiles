af5c63fe8cef4371f3afa50c796424dd485db5b0
karyanayandi <xkaryanayandi@gmail.com>
2024-12-12 19:24:50 +0700

    not update cwd when file focused

--- home/neovim/nvim/lua/plugin/tree.lua
+++ home/neovim/nvim/lua/plugin/tree.lua
@@ -92,7 +92,7 @@ return {
       },
       update_focused_file = {
         enable = true,
-        update_cwd = true,
+        update_cwd = false,
         ignore_list = {},
       },
       git = {
