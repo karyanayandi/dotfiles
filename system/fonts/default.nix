61b1286b3189dda779fb48f1fc463e82bd446513
karyanayandi <xkaryanayandi@gmail.com>
2024-12-23 12:20:36 +0700

    remove unused nerd fonts

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
-    nerd-fonts.droid-sans-mono
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-emoji
