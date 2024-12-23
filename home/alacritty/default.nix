e7a957f6e625e5084f25e0e822f9efe7a6b74c35
karyanayandi <xkaryanayandi@gmail.com>
2024-12-23 11:37:58 +0700

    update alacritty config. use xterm-256 color

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -2,9 +2,10 @@
   programs.alacritty = {
     enable = true;
     settings = {
+      env.TERM = "xterm-256color";
       window.padding = {
-        x = 2;
-        y = 2;
+        x = 4;
+        y = 4;
       };
       cursor.style = "Beam";
       terminal.shell = "${pkgs.fish}/bin/fish";
