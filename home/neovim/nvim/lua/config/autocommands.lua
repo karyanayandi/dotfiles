a811c82143c0eff5fe323111bd3aaeb34e91b76a
karyanayandi <xkaryanayandi@gmail.com>
2024-12-24 19:40:41 +0700

    feat(autocommands): update autocmd for Copilot integration
    - Changed autocmd event from "BufEnter" to "User" for Copilot buffers
    - Removed keymap setting for <C-p> in Copilot buffers
    - Ensured line numbers are disabled in Copilot buffers

--- home/neovim/nvim/lua/config/autocommands.lua
+++ home/neovim/nvim/lua/config/autocommands.lua
@@ -128,14 +128,10 @@ end, {
   bang = true,
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "User" }, {
   pattern = "copilot-*",
   callback = function()
     vim.opt_local.relativenumber = false
     vim.opt_local.number = false
-
-    vim.keymap.set("n", "<C-p>", function()
-      print(require("CopilotChat").response())
-    end, { buffer = true, remap = true })
   end,
 })
