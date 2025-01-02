ba6c7e873313e71dd31e10cebe8dc81eb6e24989
karyanayandi <xkaryanayandi@gmail.com>
2025-01-03 05:15:12 +0700

    feat(incline.lua): remove icon from filename render
    The icon and its color have been removed from the filename render
    function in incline.lua. This simplifies the display by only showing
    the filename without the icon.

--- home/neovim/nvim/lua/plugin/incline.lua
+++ home/neovim/nvim/lua/plugin/incline.lua
@@ -15,8 +15,7 @@ return {
       window = { margin = { vertical = 0, horizontal = 1 } },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-        return { { icon, guifg = color }, { " " }, { filename } }
+        return { { " " }, { filename } }
       end,
     }
   end,
