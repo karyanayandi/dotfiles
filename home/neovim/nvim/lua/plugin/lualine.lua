53d5143f876191b22ca12fd8e59a67b6dbc21ded
karyanayandi <xkaryanayandi@gmail.com>
2025-01-03 16:47:27 +0700

    feat(lualine): rearrange sections in lualine config
    Moved 'filename' from 'lualine_y' to 'lualine_z' section to improve
    status line organization and readability.

--- home/neovim/nvim/lua/plugin/lualine.lua
+++ home/neovim/nvim/lua/plugin/lualine.lua
@@ -79,8 +79,8 @@ return {
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
-        lualine_y = { diagnostics, filename },
-        lualine_z = {},
+        lualine_y = { diagnostics },
+        lualine_z = { filename },
       },
       extensions = {},
     }
