6cac78d53d9b845d8a21101e8c55dae4d90f97c5
karyanayandi <xkaryanayandi@gmail.com>
2025-01-03 18:47:29 +0700

    feat(plugin): consolidate tailwind plugins
    - Removed `tailwind-fold.lua` and `tw-values.lua`
    - Added `tailwind.lua` with combined functionality
    - Included key mappings for toggling and values

---
+++ home/neovim/nvim/lua/plugin/tailwind.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "razak17/tailwind-fold.nvim",
+    opts = {
+      symbol = "…", -- 󱏿
+      highlight = {
+        fg = "#98c379",
+      },
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = {
+      "astro",
+      "blade",
+      "edge",
+      "eruby",
+      "html",
+      "htmldjango",
+      "javascript",
+      "javascriptreact",
+      "php",
+      "svelte",
+      "tsx",
+      "typescriptreact",
+      "vue",
+    },
+    keys = {
+      { "<leader>tu", "<cmd>TailwindFoldToggle<cr>", desc = "which_key_ignore" },
+    },
+  },
+  {
+    "MaximilianLloyd/tw-values.nvim",
+    config = true,
+    lazy = true,
+    ft = { "typescript", "typescriptreact", "vue", "html", "svelte", "astro" },
+    keys = {
+      { "<leader>tw", "<cmd>TWValues<cr>", desc = "which_key_ignore" },
+    },
+    opts = {
+      show_unknown_classes = true,
+    },
+  },
+}
