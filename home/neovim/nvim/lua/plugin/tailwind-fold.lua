431ee17e07f58f26d48d9c925cd78216d605cc8a
karyanayandi <xkaryanayandi@gmail.com>
2025-01-03 17:39:13 +0700

    feat(plugin): add tailwind-fold.nvim plugin
    Added tailwind-fold.nvim plugin to enhance Tailwind CSS class folding
    support in various file types including astro, blade, edge, eruby,
    html, htmldjango, php, svelte, tsx, and vue. This plugin depends on
    nvim-treesitter.

---
+++ home/neovim/nvim/lua/plugin/tailwind-fold.lua
@@ -0,0 +1,17 @@
+return {
+  "razak17/tailwind-fold.nvim",
+  opts = {},
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = {
+    "astro",
+    "blade",
+    "edge",
+    "eruby",
+    "html",
+    "htmldjango",
+    "php",
+    "svelte",
+    "tsx",
+    "vue",
+  },
+}
