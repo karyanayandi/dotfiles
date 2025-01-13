7da9ef6234ffc284d137f36f950295de2eb769f3
karyanayandi <xkaryanayandi@gmail.com>
2025-01-13 13:51:15 +0700

    feat(flash): update key mappings and search options
    - Changed key mapping from 's' to 'f' for normal, visual, and operator modes
    - Updated 'S' to 'F' for normal, visual, and operator modes
    - Added search options to 'flash.jump' function
    - Removed unused key mappings for remote flash and treesitter search

--- home/neovim/nvim/lua/plugin/flash.lua
+++ home/neovim/nvim/lua/plugin/flash.lua
@@ -4,44 +4,24 @@ return {
   opts = {},
   keys = {
     {
-      "s",
+      "f",
       mode = { "n", "x", "o" },
       function()
-        require("flash").jump()
+        require("flash").jump {
+          search = { forward = true, wrap = false, multi_window = false },
+        }
       end,
       desc = "Flash",
     },
     {
-      "S",
+      "F",
       mode = { "n", "x", "o" },
       function()
-        require("flash").treesitter()
+        require("flash").jump {
+          search = { forward = false, wrap = false, multi_window = false },
+        }
       end,
       desc = "Flash Treesitter",
-    },
-    {
-      "r",
-      mode = "o",
-      function()
-        require("flash").remote()
-      end,
-      desc = "Remote Flash",
-    },
-    {
-      "R",
-      mode = { "o", "x" },
-      function()
-        require("flash").treesitter_search()
-      end,
-      desc = "Treesitter Search",
-    },
-    {
-      "<c-S>",
-      mode = { "c" },
-      function()
-        require("flash").toggle()
-      end,
-      desc = "Toggle Flash Search",
     },
   },
 }
