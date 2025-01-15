d221616b499ee816117282150729a661d241c484
karyanayandi <xkaryanayandi@gmail.com>
2025-01-15 11:31:39 +0700

    feat(tmux): remove unnecessary continuum boot options
    The @continuum-boot and @continuum-boot-options settings were removed
    from the tmux configuration as they are no longer needed. This change
    simplifies the configuration and reduces potential conflicts.

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -80,8 +80,6 @@ in {
         extraConfig = ''
           set -g @continuum-restore 'on'
           set -g @continuum-save-interval '60'
-          set -g @continuum-boot 'on'
-          set -g @continuum-boot-options 'foot'
         '';
       }
       {
