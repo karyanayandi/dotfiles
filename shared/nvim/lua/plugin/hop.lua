9e7156a96b58011aeb9b9aba5e9bfe8613a392a9
karyanayandi <xkaryanayandi@gmail.com>
2025-02-05 12:54:40 +0700

    feat(hop.nvim): update configuration and key mappings
    - Changed event to "BufRead" for lazy loading
    - Updated key mappings for better usability
    - Added new key mappings for HopWord, HopChar2, HopLine, and HopLineStart
    - Improved configuration setup for hop.nvim

--- shared/nvim/lua/plugin/hop.lua
+++ shared/nvim/lua/plugin/hop.lua
@@ -1,25 +1,45 @@
 -- luacheck: globals vim
+-- luacheck: ignore 631
 
 return {
+
   "brneor/hop.nvim",
   version = "*",
-  opts = {
-    keys = "etovxqpdygfblzhckisuran",
-  },
+  event = "BufRead",
   config = function()
-    local hop = require "hop"
-    local directions = require("hop.hint").HintDirection
-    vim.keymap.set("", "f", function()
-      hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = false }
-    end, { remap = true })
-    vim.keymap.set("", "F", function()
-      hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = false }
-    end, { remap = true })
-    vim.keymap.set("", "t", function()
-      hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 }
-    end, { remap = true })
-    vim.keymap.set("", "T", function()
-      hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 }
-    end, { remap = true })
+    require("hop").setup {
+      keys = "tnplvmdhsefucwyxriao",
+      multi_windows = false,
+    }
+    vim.api.nvim_set_keymap("", "s", "<cmd>HopWord<cr>", { silent = true })
+    vim.api.nvim_set_keymap("", "h", "<cmd>HopChar2<cr>", { silent = true })
+    vim.api.nvim_set_keymap("", "L", "<cmd>HopLine<cr>", { silent = true })
+    vim.api.nvim_set_keymap("", "l", "<cmd>HopLineStart<cr>", { silent = true })
+    vim.api.nvim_set_keymap(
+      "",
+      "f",
+      "<cmd>:lua require'hop'.hint_char1({direction = require'hop.hint'.HintDirection.AFTER_CURSOR,current_line_only = true})<cr>",
+      { silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "",
+      "F",
+      "<cmd>:lua require'hop'.hint_char1({direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,current_line_only = true})<cr>",
+      { silent = true }
+    )
+    vim.api.nvim_set_keymap("", "<C-w>", "<cmd>HopWordCurrentLineAC<cr>", { silent = true })
+    vim.api.nvim_set_keymap("", "<C-b>", "<cmd>HopWordCurrentLineBC<cr>", { silent = true })
+    vim.api.nvim_set_keymap(
+      "",
+      "t",
+      "<cmd>:lua require'hop'.hint_char1({direction = require'hop.hint'.HintDirection.AFTER_CURSOR,current_line_only = true,hint_offset = -1})<cr>",
+      { silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "",
+      "T",
+      "<cmd>:lua require'hop'.hint_char1({direction = require'hop.hint'.HintDirection.BEFORE_CURSOR,current_line_only = true,hint_offset = 1})<cr>",
+      { silent = true }
+    )
   end,
 }
