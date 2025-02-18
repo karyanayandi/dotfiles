a9cc2b0cf689cfe599ce7df03d54e6ccfcf4eca2
Karyana Yandi <karyana@yandi.me>
2025-02-18 20:33:39 +0700

    feat(config): update bookmarks and tailwind plugin
    - Added new bookmarks for dotfiles and Musics in gtk-3.0 config
    - Commented out tailwind-fold.nvim plugin in tailwind.lua

--- shared/config/nvim/lua/plugin/tailwind.lua
+++ shared/config/nvim/lua/plugin/tailwind.lua
@@ -1,32 +1,32 @@
 return {
-  {
-    "razak17/tailwind-fold.nvim",
-    opts = {
-      symbol = "…", -- 󱏿
-      highlight = {
-        fg = "#98c379",
-      },
-    },
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    ft = {
-      "astro",
-      "blade",
-      "edge",
-      "eruby",
-      "html",
-      "htmldjango",
-      "javascript",
-      "javascriptreact",
-      "php",
-      "svelte",
-      "tsx",
-      "typescriptreact",
-      "vue",
-    },
-    keys = {
-      { "<leader>tu", "<cmd>TailwindFoldToggle<cr>", desc = "which_key_ignore" },
-    },
-  },
+  -- {
+  --   "razak17/tailwind-fold.nvim",
+  --   opts = {
+  --     symbol = "…", -- 󱏿
+  --     highlight = {
+  --       fg = "#98c379",
+  --     },
+  --   },
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   ft = {
+  --     "astro",
+  --     "blade",
+  --     "edge",
+  --     "eruby",
+  --     "html",
+  --     "htmldjango",
+  --     "javascript",
+  --     "javascriptreact",
+  --     "php",
+  --     "svelte",
+  --     "tsx",
+  --     "typescriptreact",
+  --     "vue",
+  --   },
+  --   keys = {
+  --     { "<leader>tu", "<cmd>TailwindFoldToggle<cr>", desc = "which_key_ignore" },
+  --   },
+  -- },
   {
     "MaximilianLloyd/tw-values.nvim",
     config = true,
