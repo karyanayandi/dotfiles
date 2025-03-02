80e9258b906298ea82f9705d61886f5057f670b9
Karyana Yandi <karyana@yandi.me>
2025-03-03 02:01:02 +0700

    fix(ufo): disable folding by default and change peek keybinding
    Change foldenable from true to false to avoid automatic folding when
    opening files. Also changed the fold peek keybinding from 'K' to 'zK'
    to prevent conflicts with the hover functionality and maintain
    consistency with other folding commands.

--- shared/config/nvim/lua/plugin/ufo.lua
+++ shared/config/nvim/lua/plugin/ufo.lua
@@ -7,13 +7,13 @@ return {
     vim.o.foldcolumn = "0"
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
+    vim.o.foldenable = false
 
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
     vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
     vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
-    vim.keymap.set("n", "K", function()
+    vim.keymap.set("n", "zK", function()
       local winid = require("ufo").peekFoldedLinesUnderCursor()
       if not winid then
         vim.lsp.buf.hover()
