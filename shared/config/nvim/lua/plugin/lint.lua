e7b4a2062a61ba8e8813b8775acb68525b97041f
Karyana Yandi <karyana@yandi.me>
2025-03-05 03:47:40 +0700

    feat(lint): add auto-linting triggers and keymap
    Add automatic linting on BufEnter, BufWritePost and InsertLeave events
    to proactively check files. Also add a <leader>ll keymap for manual
    linting when needed.

--- shared/config/nvim/lua/plugin/lint.lua
+++ shared/config/nvim/lua/plugin/lint.lua
@@ -1,5 +1,5 @@
--- luacheck: globals vim
 -- TODO: add linting for github action, deno
+-- luacheck: globals vim
 
 return {
   "mfussenegger/nvim-lint",
@@ -62,5 +62,16 @@ return {
         end
       end,
     })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>ll", function()
+      lint.try_lint()
+    end, { desc = "Trigger linting for current file" })
   end,
 }
