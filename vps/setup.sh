a81aa8ebbf5e001fc4e8fdf94098602f222e6cfe
Karyana Yandi <karyana@yandi.me>
2025-06-24 21:46:54 +0700

    feat(security): add fail2ban and ufw firewall config
    Add basic security configuration for VPS setup:
    - Install fail2ban for intrusion prevention
    - Install and configure ufw firewall with common rules
    - Enable SSH, HTTP, and HTTPS ports
    - Enable services on boot

--- vps/setup.sh
+++ vps/setup.sh
@@ -4,9 +4,11 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install \
   eza \
+  fail2ban \
   fish \
   git \
   neofetch \
+  ufw \
   vim
 
 curl -sS https://starship.rs/install.sh | sh
@@ -14,3 +16,11 @@ curl -sS https://starship.rs/install.sh | sh
 cp -r . ~/
 
 chsh -s /usr/bin/fish
+
+ufw allow OpenSSH
+ufw allow ssh
+ufw allow http
+ufw allow https
+ufw enable
+
+systemctl enable fail2ban
