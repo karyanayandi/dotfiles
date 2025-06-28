79ab4f38988d760460e6d467ee7d5d40e2e8f8bd
Karyana Yandi <karyana@yandi.me>
2025-06-28 14:36:14 +0700

    fix(presence): update neovim image text for clarity
    Changed the neovim_image_text from "just VS Code without mouse" to
    "just VS Code but without mouse" in the Presence plugin configuration
    for improved clarity.

--- shared/config/nvim/lua/plugin/presence.lua
+++ shared/config/nvim/lua/plugin/presence.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("presence").setup {
       auto_update = true,
-      neovim_image_text = "just VS Code without mouse",
+      neovim_image_text = "just VS Code but without mouse",
       main_image = "neovim",
       log_level = nil,
       debounce_timeout = 10,
