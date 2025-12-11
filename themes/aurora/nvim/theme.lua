64274f8b9c2989dacb05e2711d01084d29683ee3
Yandi <karyana@yandi.me>
2025-12-12 05:24:57 +0700

    refactor(theme): update base16 and aurora config structure
    Switch theme configs to table-based plugin definitions and set
    base16-colorscheme as default install colorscheme.

--- themes/aurora/nvim/theme.lua
+++ themes/aurora/nvim/theme.lua
@@ -1,30 +1,55 @@
 -- luacheck: globals vim
 
 return {
-	"aurora-theme/nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("aurora").setup({
-			theme = nil,
-			fade_nc = true,
-			disable = {
-				background = true,
-				float_background = true,
-				cursorline = true,
-				eob_lines = false,
-			},
-			borders = true,
-			styles = {
-				comments = "italic",
-				strings = "NONE",
-				keywords = "NONE",
-				functions = "NONE",
-				variables = "NONE",
-				diagnostics = "underline",
-			},
-		})
+	{
+		"RRethy/base16-nvim",
+		config = function()
+			require("base16-colorscheme").setup({
+				base00 = "#282c34",
+				base01 = "#353b45",
+				base02 = "#3e4451",
+				base03 = "#545862",
+				base04 = "#abb2bf",
+				base05 = "#eceff4",
+				base06 = "#c8ccd4",
+				base07 = "#8fbcbb",
+				base08 = "#88c0d0",
+				base09 = "#81a1c1",
+				base0A = "#5e81ac",
+				base0B = "#bf616a",
+				base0C = "#d08770",
+				base0D = "#ebcb8b",
+				base0E = "#a3be8c",
+				base0F = "#b48ead",
+			})
+		end,
+	},
+	{
+		"aurora-theme/nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("aurora").setup({
+				theme = nil,
+				fade_nc = true,
+				disable = {
+					background = true,
+					float_background = true,
+					cursorline = true,
+					eob_lines = false,
+				},
+				borders = true,
+				styles = {
+					comments = "italic",
+					strings = "NONE",
+					keywords = "NONE",
+					functions = "NONE",
+					variables = "NONE",
+					diagnostics = "underline",
+				},
+			})
 
-		vim.cmd("colorscheme aurora")
-	end,
+			vim.cmd("colorscheme aurora")
+		end,
+	},
 }
--- themes/lackluster/nvim/theme.lua
+++ themes/lackluster/nvim/theme.lua
@@ -1,27 +1,35 @@
 -- luacheck: globals vim
 
 return {
-	"RRethy/base16-nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("base16-colorscheme").setup({
-			base00 = "#0A0A0A",
-			base01 = "#080808",
-			base02 = "#444444",
-			base03 = "#7A7A7A",
-			base04 = "#DEEEED",
-			base05 = "#DEEEED",
-			base06 = "#DEEEED",
-			base07 = "#708090",
-			base08 = "#708090",
-			base09 = "#7788AA",
-			base0A = "#7788AA",
-			base0B = "#D70000",
-			base0C = "#FFAA88",
-			base0D = "#FFAA88",
-			base0E = "#789978",
-			base0F = "#D7007D",
-		})
-	end,
+	{
+		"RRethy/base16-nvim",
+		config = function()
+			require("base16-colorscheme").setup({
+				base00 = "#0A0A0A",
+				base01 = "#080808",
+				base02 = "#444444",
+				base03 = "#7A7A7A",
+				base04 = "#DEEEED",
+				base05 = "#DEEEED",
+				base06 = "#DEEEED",
+				base07 = "#708090",
+				base08 = "#708090",
+				base09 = "#7788AA",
+				base0A = "#7788AA",
+				base0B = "#D70000",
+				base0C = "#FFAA88",
+				base0D = "#FFAA88",
+				base0E = "#789978",
+				base0F = "#D7007D",
+			})
+		end,
+	},
+	{
+		"slugbyte/lackluster.nvim",
+		lazy = false,
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("lackluster")
+		end,
+	},
 }
