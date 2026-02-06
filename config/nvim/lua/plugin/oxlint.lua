e47e11b698581eb7365476790b96461164d960a6
Yandi <karyana@yandi.me>
2026-02-06 17:21:58 +0700

    fix: oxlint lsp

--- config/nvim/lua/plugin/oxlint.lua
+++ config/nvim/lua/plugin/oxlint.lua
@@ -4,25 +4,5 @@ return {
   "soulsam480/nvim-oxlint",
   lazy = true,
   ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte", "astro" },
-  config = function()
-    require("oxlint").setup {
-      config_path = function()
-        -- Search for oxlint config files
-        if vim.fn.glob "oxlint.json" ~= "" then
-          return "oxlint.json"
-        elseif vim.fn.glob "oxlint.config.js" ~= "" then
-          return "oxlint.config.js"
-        elseif vim.fn.glob "oxlint.config.ts" ~= "" then
-          return "oxlint.config.ts"
-        elseif vim.fn.glob ".oxlint.json" ~= "" then
-          return ".oxlint.json"
-        elseif vim.fn.glob ".oxlint.jsonc" ~= "" then
-          return ".oxlint.jsonc"
-        elseif vim.fn.glob "oxc.json" ~= "" then
-          return "oxc.json"
-        end
-        return nil
-      end,
-    }
-  end,
+  opts = {},
 }
