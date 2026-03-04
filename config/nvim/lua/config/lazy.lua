f24bcfa62c4fab43e3dee8e448ad88b3915395f1
Yandi <karyana@yandi.me>
2026-03-05 02:52:32 +0700

    feat(ui): add backdrop configuration to lazy.nvim setup
    Introduce a new `ui` configuration option in the lazy.nvim setup to
    set the backdrop level. This enhances the customization of the UI
    experience.

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -33,6 +33,9 @@ require("lazy").setup {
     -- import your plugins
     { import = "plugin" },
   },
+  ui = {
+    backdrop = 100,
+  },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "base16-colorscheme" } },
