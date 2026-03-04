d5eab084f91a618904590892a3e92ca0f918ea77
Yandi <karyana@yandi.me>
2026-03-05 06:45:38 +0700

    fix(copilot): restore panel and suggestion keymaps
    Re-enabled and restored the keymap configurations for Copilot's panel
    and suggestion features. Also updated filetype support to include
    help files.

--- config/nvim/lua/plugin/copilot.lua
+++ config/nvim/lua/plugin/copilot.lua
@@ -7,34 +7,33 @@ return {
   build = ":Copilot auth",
   config = function()
     require("copilot").setup {
-      -- panel = {
-      --   enabled = false,
-      --   keymap = {
-      --     jump_next = "<c-j>",
-      --     jump_prev = "<c-k>",
-      --     accept = "<c-l>",
-      --     refresh = "r",
-      --     open = "<M-CR>",
-      --   },
-      -- },
-      -- suggestion = {
-      --   enabled = false,
-      --   auto_trigger = true,
-      --   keymap = {
-      --     accept = "<c-l>",
-      --     next = "<c-j>",
-      --     prev = "<c-k>",
-      --     dismiss = "<c-h>",
-      --   },
-      -- },
+      panel = {
+        enabled = false,
+        keymap = {
+          jump_next = "<c-j>",
+          jump_prev = "<c-k>",
+          accept = "<c-l>",
+          refresh = "r",
+          open = "<M-CR>",
+        },
+      },
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+        keymap = {
+          accept = "<c-l>",
+          next = "<c-j>",
+          prev = "<c-k>",
+          dismiss = "<c-h>",
+        },
+      },
       filetypes = {
         yaml = true,
         markdown = true,
-        help = false,
+        help = true,
         gitcommit = false,
         gitrebase = false,
         cvs = false,
-        ["."] = false,
       },
       copilot_node_command = "node",
       server_opts_overrides = {},
