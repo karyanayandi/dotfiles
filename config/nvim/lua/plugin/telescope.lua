6b88b453eb6fb15b56afb5f572b7a50033628134
Yandi <karyana@yandi.me>
2026-03-05 23:38:22 +0700

    feat(telescope): add install target to fzf-native build

--- config/nvim/lua/plugin/telescope.lua
+++ config/nvim/lua/plugin/telescope.lua
@@ -7,7 +7,7 @@ return {
   dependencies = {
     {
       "nvim-telescope/telescope-fzf-native.nvim",
-      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
       config = function()
         require("telescope").load_extension "fzf"
       end,
