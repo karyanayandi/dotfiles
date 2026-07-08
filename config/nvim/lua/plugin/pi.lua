8afbe2e54c375233a4fdabe356e7e04e92d7f088
Karyana Yandi <karyana@yandi.me>
2026-07-08 17:42:05 +0700

    feat(pi): add new keymaps for Pi plugin actions

--- config/nvim/lua/plugin/pi.lua
+++ config/nvim/lua/plugin/pi.lua
@@ -9,6 +9,13 @@ return {
 
   config = function(_, opts)
     require("pi-nvim").setup(opts)
-    vim.keymap.set({ "n", "v" }, "<leader>as", ":Pi<CR>", { desc = "Send to pi" })
+
+    vim.keymap.set({ "n", "v" }, "<leader>as", "<cmd>Pi<CR>", { desc = "Pi dialog" })
+    vim.keymap.set("n", "<leader>aa", "<cmd>PiSend<CR>", { desc = "Pi send prompt" })
+    vim.keymap.set("n", "<leader>af", "<cmd>PiSendFile<CR>", { desc = "Pi send file" })
+    vim.keymap.set("v", "<leader>av", "<cmd>PiSendSelection<CR>", { desc = "Pi send selection" })
+    vim.keymap.set("n", "<leader>ab", "<cmd>PiSendBuffer<CR>", { desc = "Pi send buffer" })
+    vim.keymap.set("n", "<leader>ap", "<cmd>PiPing<CR>", { desc = "Pi ping" })
+    vim.keymap.set("n", "<leader>aS", "<cmd>PiSessions<CR>", { desc = "Pi sessions" })
   end,
 }
