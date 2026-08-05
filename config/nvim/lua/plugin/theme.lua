08474a9b6862f30a09f64011f4df1f616ea1321f
Karyana Yandi <karyana@yandi.me>
2026-08-05 14:23:39 +0700

    feat(theme): add highlights for neotest integration
    Base16 theme now links neotest groups to existing highlight groups.

--- config/nvim/lua/plugin/theme.lua
+++ config/nvim/lua/plugin/theme.lua
@@ -5,6 +5,23 @@ return {
     priority = 10,
     config = function()
       vim.cmd "colorscheme base16-gruvbox-dark-hard"
+      local hl = vim.api.nvim_set_hl
+      -- base16-nvim doesn't theme neotest; wire it to existing groups
+      -- https://github.com/nvim-neotest/neotest
+      hl(0, "NeotestPassed", { link = "DiagnosticOk" })
+      hl(0, "NeotestFailed", { link = "DiagnosticError" })
+      hl(0, "NeotestRunning", { link = "DiagnosticWarn" })
+      hl(0, "NeotestSkipped", { link = "DiagnosticInfo" })
+      hl(0, "NeotestUnknown", { link = "Comment" })
+      hl(0, "NeotestFocused", { link = "Title", bold = true })
+      hl(0, "NeotestMarked", { link = "MoreMsg" })
+      hl(0, "NeotestFile", { link = "Directory" })
+      hl(0, "NeotestDir", { link = "Directory" })
+      hl(0, "NeotestNamespace", { link = "Keyword" })
+      hl(0, "NeotestAdapterName", { link = "Type" })
+      hl(0, "NeotestTest", { link = "Normal" })
+      hl(0, "NeotestIndent", { link = "Normal" })
+      hl(0, "NeotestExpandMarker", { link = "Normal" })
     end,
   },
 }
